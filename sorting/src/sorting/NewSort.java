965344c3ec56be3863c3634680411d5aeda46969
venkata.chandu <venkata.chandu@CSELAB02-13.bennett.edu.in>
2017-08-22 11:55:36 +0530

    commit

--- sorting/src/sorting/NewSort.java
+++ sorting/src/sorting/NewSort.java
@@ -1,9 +1,16 @@
 package sorting;
 
+import java.util.Arrays;
+
 public class NewSort {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int a[] = {2,5,7,3,8,11,51};
+		Arrays.sort(a);
+		for(int i=0;i<a.length;i++)
+		System.out.println(a[i]);
+		
 
 	}
 
